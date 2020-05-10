e78ec4cba026c3fbd0e286cdcc1ae05d014bc94f
xmly <gyue@outlook.com>
2020-05-09 22:24:43 -0700

    Update the node to 12 minimum
    Add the stacklint.test.ts

---
+++ cli/test/lib/stacklint.test.ts
@@ -0,0 +1,8 @@
+const assert = require('assert')
+describe('StackLint Run', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.strict.equal([1, 2, 3].indexOf(4), -1)
+    })
+  })
+})
