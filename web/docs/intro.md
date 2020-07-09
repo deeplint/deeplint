19a4be4408b079bfdbcde0f12e00792a4f19e15d
xmly <gyue@outlook.com>
2020-07-09 16:49:12 -0700

    unfinished work on docs

--- web/docs/intro.md
+++ web/docs/intro.md
@@ -2,36 +2,39 @@
 title: Introduction
 sidebar_label: Introduction
 ---
-
 ## What is DeepLint?
-[DeepLint](https://deeplint.com) is an open-source Security-as-Code framework for securing and optimizing any cloud, infrastructure or services. 
 
-With DeepLint, you can build,enforce and share security workflows as code to find, fix and prevent costly mistakes in any cloud configurations, 
-or use pre-curated industrial best practices to quickly elevate cloud security posture.
+[DeepLint](https://deeplint.com) is an open-source Cloud Security-as-Code framework for securing and optimizing any cloud, infrastructure, or services. With DeepLint, you can build, enforce, and share security workflows as code to find, fix, and prevent costly mistakes in cloud configurations. 
+
+With pluggable security packages, DeepLint can be easily extended to check multiple cloud accounts and work with any cloud types, from IaaS to SaaS, as well as Infrastructure-as-Code.
 
 ## Why DeepLint?
 
-Organizations are quickly moving to Cloud for its agility, scalability and cost-effectiveness. However, cloud also brings chaos. 
+Organizations are quickly moving to Cloud for agility, scalability, and cost-effectiveness. However, Cloud can also bring chaos. A simple configuration mistake can cause data leakage or leave idle resources that cost you a fortune. 
+
+DeepLint adopts the Security-as-Code approach and aims to provide a unified and extendable cloud security tool to help organizations better utilize Cloud without slowing down product development and business growth.
 
 ### Security as Code
 
-Codify security workflows as code for a repeated and consistent usage, as well as version control, peer review and sharing.
+Codify security workflows for repeated and consistent usage. Manage security as code - version control, peer review, and sharing.
 
-### DevSecOps
-Embed security checks into DevOps pipelines to enforce high standards without slowing down development.
+### Extendable
+
+Package and share your security best practices within the team. Use pre-curated security packages to elevate cloud security posture quickly.
 
 ### Multi-Cloud
-Use one unified tool to check multiple cloud accounts and infrastructure as code with pluggable security packages.
+
+Use one unified tool to check multiple Cloud and Infrastructure-as-Code with easy-to-configure security packages.
+
+### DevSecOps
+
+Embed security workflows into DevOps pipelines to automate cloud security without slowing down development.
+
 
 ## How it works
 
+### Build
 
+### Scan
 
-## Features
-
-### Self-contained Package
-
-### Resource Snapshot
-
-### Actionable Advice
-
+### Check and Fix
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -9,12 +9,12 @@ import Head from '@docusaurus/Head';
 
 function Home() {
     const context = useDocusaurusContext();
-    const {siteConfig = {}} = context;
+    const { siteConfig = {} } = context;
     return (
         <Layout>
             <Head>
                 <meta name="description"
-                      content="DeepLint is an open-source Security-as-Code framework for securing and optimizing any cloud, infrastructure or services.
+                    content="DeepLint is an open-source Security-as-Code framework for securing and optimizing any cloud, infrastructure or services.
                       With DeepLint, you can build and enforce security workflows as code to enhance security, reduce cost and stay compliant in your cloud environments."/>
             </Head>
             <header className={clsx('hero', styles.heroBanner)}>
@@ -22,8 +22,8 @@ function Home() {
                     <div className="row">
                         <div className="col col--6">
                             <div>
-                                <h1>Prevent and fix problems in Cloud configurations</h1>
-                                <p>Use <strong>Security-as-Code</strong> to secure and optimize any Cloud, infrastructure, or services.</p>
+                                <h1>DeepLint</h1>
+                                <p>Use <strong>Security-as-Code</strong> to find, fix and prevent costly configuration mistakes in any Cloud, infrastructure, or services.</p>
                                 <p>DeepLint protects your cloud environments from:</p>
                                 <div className="row">
                                     <div className="col">
@@ -45,7 +45,7 @@ function Home() {
                             </div>
                         </div>
                         <div className="col col--offset-1 col--5">
-                            <div><img src="img/top_banner.svg"/></div>
+                            <div><img src="img/top_banner.svg" /></div>
                         </div>
                     </div>
                 </div>
@@ -56,42 +56,40 @@ function Home() {
                         <div className={clsx('row', styles.sectionHeader)}>
                             <div className="col col--12 text--center">
                                 <h2>Automated cloud security and excellence with DeepLint</h2>
-                                <p>DeepLint is an open-source Security-as-Code framework that enables you build, enforce and share security workflows as code to find, fix and prevent costly mistakes in any
-                                    cloud configurations through entire DevOps lifecycle.</p>
+                                <p>DeepLint is an open-source Security-as-Code framework that enables you build, enforce and share security workflows as code
+                                    to secure and optimize cloud environments through entire DevOps lifecycle.</p>
                             </div>
                         </div>
                         <div className="row">
                             <div className="col col--6">
                                 <div className={styles.feature}>
-                                    <div><img src="img/policy.svg"/></div>
+                                    <div><img src="img/policy.svg" /></div>
                                     <h3>Security as Code</h3>
-                                    <p>Codify and automate security workflows as code - version control, peer review,
-                                        enforce and share.
+                                    <p>Codify security workflows for a repeated and consistent usage. Manage security as code - version control, peer review and sharing.
                                     </p>
                                 </div>
                             </div>
                             <div className="col col--6">
                                 <div className={styles.feature}>
-                                    <div><img src="img/file.svg"/></div>
-                                    <h3>DevSecOps</h3>
-                                    <p>Embed security checks into DevOps pipelines to enforce high
-                                        standards without slowing down development. </p>
+                                    <div><img src="img/trust.svg" /></div>
+                                    <h3>Extendable</h3>
+                                    <p>Package and share your security best practices within the team.
+                                    Use pre-curated security packages to elevate cloud security posture quickly.
+                                    </p>
                                 </div>
                             </div>
                             <div className="col col--6">
                                 <div className={styles.feature}>
-                                    <div><img src="img/cloud.svg"/></div>
+                                    <div><img src="img/cloud.svg" /></div>      
                                     <h3>Multi Cloud</h3>
-                                    <p>Use one unified tool to check multiple cloud accounts and
-                                        infrastructure as code with pluggable security packages.</p>
+                                    <p>Use one unified tool to check multiple Cloud and Infrastructure-as-Code with easy-to-configure packages.</p>
                                 </div>
                             </div>
                             <div className="col col--6">
                                 <div className={styles.feature}>
-                                    <div><img src="img/trust.svg"/></div>
-                                    <h3>Collaboration</h3>
-                                    <p>Share your security best practices within team, or use pre-curated security packages to
-                                        quickly elevate cloud security posture.</p>
+                                    <div><img src="img/file.svg" /></div>
+                                    <h3>DevSecOps</h3>
+                                    <p>Embed security workflows into DevOps pipelines to automate cloud security without slowing down development.</p>
                                 </div>
                             </div>
                         </div>
